9d252ae62fa94538ff7453cd827d041847d764a0
Tomislav Mikšik <64797760+tomislavmiksik@users.noreply.github.com>
2022-08-28 15:23:21 +0200

    init commit

---
+++ app/src/main/java/hr/tvz/android/zavrsniprojektmiksik/ui/main/SectionsPagerAdapter.kt
@@ -0,0 +1,28 @@
+package hr.tvz.android.zavrsniprojektmiksik.ui.main
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import hr.tvz.android.zavrsniprojektmiksik.R
+
+private val TAB_TITLES = arrayOf(
+    R.string.tab_text_1,
+    R.string.tab_text_2,
+    R.string.tab_text_3,
+)
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return PlaceholderFragment.newInstance(position + 1)
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return context.resources.getString(TAB_TITLES[position])
+    }
+
+    override fun getCount(): Int {
+        return 3
+    }
+}
