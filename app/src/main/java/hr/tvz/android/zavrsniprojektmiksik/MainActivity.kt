f610ef2bf55b2be40dc6a965876b0b37191bc894
Tomislav Mikšik <tomislav.miksik@gmail.com>
2022-09-01 09:11:16 +0200

    further cleanup

--- app/src/main/java/hr/tvz/android/zavrsniprojektmiksik/MainActivity.kt
+++ app/src/main/java/hr/tvz/android/zavrsniprojektmiksik/MainActivity.kt
@@ -1,31 +1,16 @@
 package hr.tvz.android.zavrsniprojektmiksik
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager.widget.ViewPager
 import com.facebook.drawee.backends.pipeline.Fresco
 import com.google.android.material.tabs.TabLayout
-import hr.tvz.android.zavrsniprojektmiksik.database.enitites.DriverEntity
-import hr.tvz.android.zavrsniprojektmiksik.database.enitites.RaceEntity
-import hr.tvz.android.zavrsniprojektmiksik.database.enitites.TeamEntity
-import hr.tvz.android.zavrsniprojektmiksik.database.viewmodel.DriverViewModel
-import hr.tvz.android.zavrsniprojektmiksik.database.viewmodel.RaceViewModel
-import hr.tvz.android.zavrsniprojektmiksik.database.viewmodel.TeamViewModel
 import hr.tvz.android.zavrsniprojektmiksik.databinding.ActivityMainBinding
-import hr.tvz.android.zavrsniprojektmiksik.services.ServiceGenerator
-import hr.tvz.android.zavrsniprojektmiksik.services.ServiceInterface
 import hr.tvz.android.zavrsniprojektmiksik.ui.main.SectionsPagerAdapter
-import retrofit2.Call
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    lateinit var raceViewModel: RaceViewModel
-    lateinit var teamViewModel: TeamViewModel
-    lateinit var driverViewModel: DriverViewModel
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
